51e6327e59dde9c9d292b29e3ea634efdb38c3ed
PhamNhatKhanh <khanhhcm4@gmail.com>
2021-07-20 06:56:23 +0700

    push code

---
+++ z_rut.js
@@ -0,0 +1,14 @@
+ /*
+LOCATION: 
+*Center:
+ - position:absolute + top + transform(fixed although use %)
+ - In div have many tag: -> center 
+
+
+
+ *Z-index:
+ - when active but something not active ->
+    (? In flie Css: if fashion-box-title use z_index -> div is overwrite by class after and before -> Why.
+        ! Attendtion if not set background-color -> div is still overwrite.
+
+ */
